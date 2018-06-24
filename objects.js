8fe8a8caf70308597bebbd035a443028dea1d845
Richard Chen <richard.c.chen@gmail.com>
2018-06-24 23:57:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Slowdive: "Alison"};
 
 function updatePlaylist(playlist, artistname, songtitle){
-  playlist['artistname'] = 'songtitle';
+  playlist[artistname] = songtitle;
   return playlist
 }
