59ddaffdcf8dae14e8b454356105023a8cb55670
Richard Chen <richard.c.chen@gmail.com>
2018-06-24 23:54:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist: "song"};
 
-function updatePlaylist(playlist, artist, song){
-  playlist['artist'] = 'song';
+function updatePlaylist(playlist, artistname, songtitle){
+  playlist['artistname'] = 'songtitle';
   return playlist
 }
